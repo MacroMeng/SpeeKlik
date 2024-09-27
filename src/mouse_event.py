0bb85bf666b4633f92c85df0275305e60eb068d0
Macros <Meng-yc@outlook.com>
2024-09-27 19:10:17 +0800

    Add __repr__/__str__ for mouse_event.py Fix an error `MouseEvent.(self.)events may be None`

--- src/mouse_event.py
+++ src/mouse_event.py
@@ -1,13 +1,12 @@
 from collections.abc import Callable
 
-
 Pos = tuple[int, int]
 
 
 class MouseEvents:
     def __init__(self, events: list[Pos] | None = None):
         if events is None:
-            self.events = []
+            events = []
         self.events = events
 
     def add_pos(self, pos: Pos):
@@ -16,3 +15,11 @@ class MouseEvents:
     def run_all(self, caller: Callable[[Pos], None]):
         for event in self.events:
             caller(event)
+
+    def __repr__(self):
+        key_lists_str = str(self.events)
+        return f"{self.__class__.__name__}({key_lists_str})"
+
+    def __str__(self):
+        res = ";".join(f"({event[0]}, {event[1]})" for event in self.events)
+        return res
