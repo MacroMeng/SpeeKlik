9614622322a083d48daf2f5b43ed2a72a227b3ca
Macros Meng <Meng-yc@outlook.com>
2025-01-28 16:54:21 +0800

    用来运行MouseKeyEvents的mouse_funx.run函数写完了（？）

--- src/mouse_funcs.py
+++ src/mouse_funcs.py
@@ -1,7 +1,7 @@
 """为主程序main()提供处理鼠标/键盘的函数"""
 import functools
 from tkinter import messagebox
-from typing import Callable
+from typing import Callable, Any
 
 import pyautogui as pag
 
@@ -52,12 +52,36 @@ def safe_pause_val_wrapper(new_safe_delay: float):
     return do_wrap
 
 
+def failsafe_val_wrapper(new_failsafe: float) -> Callable[[Any], Any]:
+    """一个装饰器，可以在被装饰的函数内部重写pyautogui.PAUSE。"""
+    def do_wrap(to_wrap: Callable):
+        @functools.wraps(to_wrap)
+        def wrapper(*args, **kwargs):
+            org = pag.FAILSAFE
+            pag.FAILSAFE = new_failsafe
+            try:
+                to_wrap(*args, **kwargs)
+            finally:
+                pag.FAILSAFE = org
+        return wrapper
+    return do_wrap
+
+
 def run(command: me.MouseKeyEvents) -> None:
     env = command.env
-    for event in command:
-        if isinstance(event, me.MouseClick):
-            pag.sleep(event.delay if event.delay is not None else env.delay)
-            pag.click(event.x, event.y)
-        elif isinstance(event, me.KeyClick):
-            pag.sleep(event.delay if event.delay is not None else env.delay)
-            pag.press()
+
+    @safe_pause_val_wrapper(env.safe_delay)
+    @failsafe_val_wrapper(env.failsafe)
+    def work():
+        for event in command:
+            if isinstance(event, me.MouseClick):
+                button_map = {me.MouseClickButtons.LEFT: pag.LEFT,
+                              me.MouseClickButtons.RIGHT: pag.RIGHT,
+                              me.MouseClickButtons.MIDDLE: pag.MIDDLE}
+                button = button_map[event.button]
+                pag.sleep(event.delay if event.delay is not None else env.delay)
+                pag.click(event.x, event.y, button=button)
+            elif isinstance(event, me.KeyClick):
+                pag.sleep(event.delay if event.delay is not None else env.delay)
+                pag.press(event.key)
+    work()
